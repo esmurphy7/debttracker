90a57e8bde44aa6349276b3be1b0e6f430310653
Evan Murphy <esmurphy7@gmail.com>
2015-05-21 10:27:46 -0700

    Client simulator project - console app and test data insertion

---
+++ ClientSimulator/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceClient 
+             //   = new MobileServiceClient("http://localhost:50780/");
+             = new MobileServiceClient("https://debttracker.azure-mobile.net/","IfsLQMYiUHObNtCttJMvYenStchggQ16");
+
+            var todoItemsTable = serviceClient.GetTable<TodoItem>();
+            todoItemsTable.InsertAsync(new TodoItem()
+                {
+                    Text = "from console app",
+                    Complete = false
+                });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClientSimulator/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSimulator
+{
+    public class TodoItem
+    {
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName="Text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "Complete")]
+        public bool Complete { get; set; }
+    }
+}
